5e5626aaad7f7f15b581c3d4aa4f116ec3b42ef6
Skave-a <arrowtata@gmail.com>
2023-02-22 18:38:09 +0300

    feat: add route

--- forest-resort/src/App.tsx
+++ forest-resort/src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter,
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Rooms } from "./pages/Rooms";
+import { SingleRoom } from "./pages/SingleRoom";
+import { Home } from "./pages/Home";
+import { Error } from "./pages/Error";
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/single-room" element={<SingleRoom />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
